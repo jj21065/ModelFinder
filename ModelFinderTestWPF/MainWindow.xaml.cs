2e30244c904cbcf3bcbf679a6b1fbe664f9ca268
jj21065 <jj21065@gmail.com>
2021-01-18 22:51:08 +0800

    add pyramid search algorithms. 1. create pyramid structure model; 2. Find models in new image transfer into pyramid structure

--- ModelFinderTestWPF/MainWindow.xaml.cs
+++ ModelFinderTestWPF/MainWindow.xaml.cs
@@ -27,12 +27,12 @@ namespace ModelFinderTestWPF
         {
             InitializeComponent();
 
-            Bitmap image = new Bitmap("template1.jpg");
+            Bitmap image = new Bitmap(@".\\template1.jpg");
             if (image != null)
             {
-                modelFinder.SetModelPara(-15, 15, 0.1, 0.6);
-                modelFinder.SetROI(200, 100, 300, 200);
-                modelFinder.SetSobelThreshold(160, 200);
+                modelFinder.SetModelPara(-5, 5, 0.1, 0.6);
+                modelFinder.SetROI(1100, 850, 400, 400);
+                modelFinder.SetSobelThreshold(120, 130);
                 modelFinder.showCvImage = true;
                 modelFinder.CreateModelFromImage(image);
                 image.Dispose();
@@ -41,7 +41,7 @@ namespace ModelFinderTestWPF
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Bitmap image2 = new Bitmap("白光HR點膠上方正面.tif");
+            Bitmap image2 = new Bitmap(".//Test_Golden.tif");
                 if(image2 != null)
                     modelFinder.ModelFind(image2);
        
@@ -49,6 +49,22 @@ namespace ModelFinderTestWPF
           
             image2.Dispose();
             //			std::cout <<std::endl << "ModelFind at (" << modelFinder->searchResult.X << "< " << modelFinder->searchResult.Y << <<")"<<std::endl;
+        }
+
+        private void SaveClick(object sender, RoutedEventArgs e)
+        {
+            modelFinder.SerializeModel(@"test.mod");
+        }
+
+        private void LoadClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                modelFinder.DeSerializeModel(@"test.mod");
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
